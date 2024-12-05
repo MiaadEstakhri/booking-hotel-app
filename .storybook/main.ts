03b275ae48affa0b286dfd4e75a3b6e57bf3bddb
MiaadEstakhri <miaad8104@gmail.com>
2024-12-05 16:34:24 +0330

    create projects

---
+++ .storybook/main.ts
@@ -0,0 +1,11 @@
+import { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  stories: [
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+    "../src/**/*.{js,jsx,ts,tsx}",
+  ],
+  framework: "@storybook/react-vite",
+};
+
+export default config;
