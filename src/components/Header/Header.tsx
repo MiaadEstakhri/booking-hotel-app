9e943c8e9e88c91bed5fd5937c8da3cb7bdade23
MiaadEstakhri <miaad8104@gmail.com>
2024-12-09 23:48:24 +0330

    add code

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,30 @@
 import React, { useState } from "react";
-import { CalenderIcon, LocationIcon, SearchIcon } from "../../assets/icons";
+import {
+  CalenderIcon,
+  LocationIcon,
+  MinusIcon,
+  PlusIcon,
+  SearchIcon,
+} from "../../assets/icons";
+import { OptionItemType, OptionsType } from "./Header.type";
 
 export default function Header() {
   const [destination, setDestination] = useState<string>("");
+  const [isOpenOption, setIsOpenOption] = useState<boolean>(false);
+  const [options, setOptions] = useState<OptionsType>({
+    Adult: 1,
+    Children: 0,
+    Room: 1,
+  });
+
+  const handleOptions = (name: string, operation: string) => {
+    setOptions((prev) => {
+      return {
+        ...prev,
+        [name]: operation === "inc" ? options[name] + 1 : options[name] - 1,
+      };
+    });
+  };
 
   return (
     <div className=" m-5">
@@ -29,8 +51,21 @@ export default function Header() {
         </div>
         <div className="grid grid-cols-3">
           <span className="justify-self-start border  mx-4 h-10 "></span>
-          <div className=" flex  items-center text-nowrap">
-            1 adult . 2 children . 1 room
+          <div className="flex ">
+            <div
+              className=" flex  items-center text-nowrap cursor-pointer"
+              onClick={() => setIsOpenOption(!isOpenOption)}
+            >
+              {`Adult : ${options.Adult} , Children : ${options.Children} , room : ${options.Room}`}
+            </div>
+            <>
+              {isOpenOption && (
+                <GuestOptionList
+                  options={options}
+                  handleOptions={handleOptions}
+                />
+              )}
+            </>
           </div>
         </div>
         <div className="grid grid-cols-3">
@@ -41,6 +76,62 @@ export default function Header() {
             </button>
           </div>
         </div>
+      </div>
+    </div>
+  );
+}
+
+function GuestOptionList({ options, handleOptions }) {
+  return (
+    <div className="absolute w-60 bg-white shadow-xl top-20 py-3 px-4 z-40 rounded-lg">
+      <OptionItem
+        type="Adult"
+        options={options}
+        minLimit={1}
+        handleOptions={handleOptions}
+      />
+      <OptionItem
+        type="Children"
+        options={options}
+        minLimit={0}
+        handleOptions={handleOptions}
+      />
+      <OptionItem
+        type="Room"
+        options={options}
+        minLimit={1}
+        handleOptions={handleOptions}
+      />
+    </div>
+  );
+}
+
+function OptionItem({
+  type,
+  options,
+  minLimit,
+  handleOptions,
+}: OptionItemType) {
+  return (
+    <div className="flex justify-between items-center mb-2">
+      <div className="">{type}</div>
+      <div className="flex items-center justify-between w-24 ">
+        <button
+          className={`${
+            options[type] <= minLimit ? "cursor-not-allowed" : "cursor-pointer"
+          } bg-slate-100 p-1 rounded-md`}
+          disabled={options[type] <= minLimit}
+          onClick={() => handleOptions(type, "dec")}
+        >
+          <MinusIcon className="h-5 w-4" />
+        </button>
+        <span className="">{options[type]}</span>
+        <button
+          className="bg-slate-100 p-1 rounded-md"
+          onClick={() => handleOptions(type, "inc")}
+        >
+          <PlusIcon className="h-4 w-4" />
+        </button>
       </div>
     </div>
   );
