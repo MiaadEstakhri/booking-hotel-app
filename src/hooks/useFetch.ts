6ec72b89e57f508bdbe4f5cae737d417ba2e69ea
MiaadEstakhri <miaad8104@gmail.com>
2024-12-14 23:26:31 +0330

    add useFetch

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+
+export default function useFetch(url: string, query = "") {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setIsLoading(true);
+        const { data } = await axios.get(`${url}?${query}`);
+        setData(data);
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          setData([]);
+          toast.error(error.message);
+          console.error("An unexpected error occurred");
+          toast.error("An unexpected error occurred");
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchData();
+  }, [url, query]);
+
+  return [isLoading, data];
+}
