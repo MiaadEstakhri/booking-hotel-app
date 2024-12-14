76ab2ad5aeeba03890f5371a9ee05282cf847634
MiaadEstakhri <miaad8104@gmail.com>
2024-12-14 23:05:32 +0330

    add range type

--- src/components/Header/Header.type.ts
+++ src/components/Header/Header.type.ts
@@ -15,3 +15,15 @@ export type GuestOptionListType = {
   handleOptions: HandleOptionsType;
   setIsOpenOption: React.Dispatch<React.SetStateAction<boolean>>;
 };
+
+export type DateRangeTypes = {
+  date: {
+    startDate: Date;
+    endDate: Date;
+    key: string;
+  }[];
+  setDate: (
+    newDates: { startDate: Date; endDate: Date; key: string }[]
+  ) => void;
+  setIsOpenDate: React.Dispatch<React.SetStateAction<boolean>>;
+};
