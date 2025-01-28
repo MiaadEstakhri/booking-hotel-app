a658283f23752e6c063dbde4419d7fc8a3d111fa
MiaadEstakhri <miaad8104@gmail.com>
2025-01-28 21:49:11 +0330

    use router

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import "./App.css";
 import { Toaster } from "react-hot-toast";
 import Header from "./components/Header/Header";
 import LocationList from "./components/LocationList/LocationList";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="m-5">
       <Toaster />
       <Header />
-      <LocationList />
+      <Routes>
+        <Route path="/" element={<LocationList />} />
+      </Routes>
     </div>
   );
 }
