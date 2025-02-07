22aa7ddad57c4eeba05789f1fee99250de1eb33a
MiaadEstakhri <miaad8104@gmail.com>
2025-02-07 21:46:31 +0330

    added appLayout

---
+++ src/components/AppLayout/AppLayout.tsx
@@ -0,0 +1,8 @@
+export default function AppLayout() {
+  return (
+    <div className="grid grid-cols-12">
+      <div className="col-span-5">sidebar</div>
+      <div className="col-span-7">map</div>
+    </div>
+  );
+}
