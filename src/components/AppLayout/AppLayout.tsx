a25b74895d8c253c9eaabdb53cee7be8f6e43f0c
MiaadEstakhri <miaad8104@gmail.com>
2025-04-07 20:48:59 +0330

    using the outlet

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -1,7 +1,11 @@
+import { Outlet } from "react-router-dom";
+
 export default function AppLayout() {
   return (
     <div className="grid grid-cols-12">
-      <div className="col-span-5">sidebar</div>
+      <div className="col-span-5">
+        <Outlet />
+      </div>
       <div className="col-span-7">map</div>
     </div>
   );
