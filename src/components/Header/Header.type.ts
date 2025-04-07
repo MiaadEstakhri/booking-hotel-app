30df321961435a58849fb66d0c3d051e944215ae
MiaadEstakhri <miaad8104@gmail.com>
2025-04-07 20:59:56 +0330

    Making DateTypes for state date and DateRangeTypes for DateRange

--- src/components/Header/Header.type.ts
+++ src/components/Header/Header.type.ts
@@ -16,14 +16,14 @@ export type GuestOptionListType = {
   setIsOpenOption: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-export type DateRangeTypes = {
-  date: {
-    startDate: Date;
-    endDate: Date;
-    key: string;
-  }[];
-  setDate: (
-    newDates: { startDate: Date; endDate: Date; key: string }[]
-  ) => void;
-  setIsOpenDate: React.Dispatch<React.SetStateAction<boolean>>;
+export type DateRangeTypes = Array<{
+  startDate: Date;
+  endDate: Date;
+  key: string;
+}>;
+
+export type HandelDateRangeTypes = {
+  date: DateRangeTypes;
+  setDate: (date: DateRangeTypes) => void;
+  setIsOpenDate: (isOpen: boolean) => void;
 };
