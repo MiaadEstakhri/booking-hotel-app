cc1e1f27f623eb21a6deb7dd5723e086f0917dbc
MiaadEstakhri <miaad8104@gmail.com>
2025-04-07 20:48:05 +0330

    Using the main tag instead of div tag

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,21 @@ import Header from "./components/Header/Header";
 import LocationList from "./components/LocationList/LocationList";
 import { Route, Routes } from "react-router-dom";
 import AppLayout from "./components/AppLayout/AppLayout";
+import Hotels from "./components/Hotels/Hotels";
 
 function App() {
   return (
-    <div className="m-5">
+    <main className="m-5">
       <Toaster />
       <Header />
       <Routes>
         <Route path="/" element={<LocationList />} />
         <Route path="/hotels" element={<AppLayout />}>
-          <Route index element={<div>hotels</div>} />
+          <Route index element={<Hotels />} />
           <Route path=":id" element={<div></div>} />
         </Route>
       </Routes>
-    </div>
+    </main>
   );
 }
 
