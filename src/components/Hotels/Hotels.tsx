5a48e8032eda334ad69ac7cdfe7b3225ce467654
MiaadEstakhri <miaad8104@gmail.com>
2025-04-19 23:32:48 +0330

    Using searchParams and useFetch

--- src/components/Hotels/Hotels.tsx
+++ src/components/Hotels/Hotels.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { useSearchParams } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
 
 function Hotels() {
-  return <div>Hotels</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const destination = searchParams.get("destination");
+  const room = JSON.parse(searchParams.get("options") || "")?.Room;
+  const { isLoading, data } = useFetch(
+    "http://localhost:3000/hotels",
+    `host_location=${destination || ""}&name_like=${
+      destination || ""
+    }&accommodates_gts=${room || 1}`
+  );
+  if (isLoading) <div>Loading...</div>;
+  return <div>{data.length}</div>;
 }
 
 export default Hotels;
