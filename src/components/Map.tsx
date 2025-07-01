961e18b4d5eff629bbbf3163ce6158336e38ee4b
MiaadEstakhri <miaad8104@gmail.com>
2025-07-01 17:19:03 +0330

    mapping on latitude and longitude to show all locations

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,26 +1,28 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { useHotels } from "./context/hotelsProvider";
+import { useState } from "react";
 
 function Map() {
   const { isLoading, hotels } = useHotels();
-  const position = [51.505, -0.09];
-
+  const [mapCenter] = useState<[number, number]>([20, 3]);
   return (
     <MapContainer
-      center={position}
+      center={mapCenter}
       zoom={13}
-      scrollWheelZoom={false}
+      scrollWheelZoom={true}
       className="h-[300px] md:h-[700px] w-full rounded-xl md:ms-3"
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
       />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      {hotels.map(({ id, latitude, longitude, host_location }) => {
+        return (
+          <Marker key={id} position={[latitude as number, longitude as number]}>
+            <Popup>{host_location}</Popup>
+          </Marker>
+        );
+      })}
     </MapContainer>
   );
 }
