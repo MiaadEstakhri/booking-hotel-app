7874e0e02b140891cdfa24eba512a3dbe4295390
MiaadEstakhri <miaad8104@gmail.com>
2025-07-04 18:44:16 +0330

    using hotels in useHotel for Map

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -1,14 +1,16 @@
 import { Outlet } from "react-router-dom";
+import { useHotels } from "../context/hotelsProvider";
 import Map from "../Map/Map";
 
 export default function AppLayout() {
+  const { hotels } = useHotels();
   return (
     <div className="grid grid-cols-12 mt-8 ">
       <div className="col-span-12 mb-5 md:mb-0 md:col-span-5 max-h-[800px] overflow-y-auto">
         <Outlet />
       </div>
       <div className="col-span-12 md:col-span-7 w-full h-full">
-        <Map />
+        <Map markerLocations={hotels} />
       </div>
     </div>
   );
