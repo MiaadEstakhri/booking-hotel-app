00c31e9e327c99ab721f2fa249289201f5588837
MiaadEstakhri <miaad8104@gmail.com>
2025-07-04 17:32:54 +0330

    current hotel as context

--- src/components/context/hotelsProvider.tsx
+++ src/components/context/hotelsProvider.tsx
@@ -1,30 +1,70 @@
-import { useContext, createContext } from "react";
+import { useContext, createContext, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import useFetch from "../../hooks/useFetch";
 import { HotelTypes } from "../LocationList/LocationList.type";
+import toast from "react-hot-toast";
+import { SingleHotelTypes } from "../SingleHotel/SingleHotel.type";
 
 type HotelsContextType = {
   isLoading: boolean;
   hotels: HotelTypes[];
+  getHotel: (id: string | number) => Promise<void>;
+  isLoadingCurrentHotel: boolean;
+  currentHotel: SingleHotelTypes | null;
 };
 
 const HotelsContext = createContext<HotelsContextType>({
   isLoading: false,
   hotels: [],
+  getHotel: async () => {},
+  isLoadingCurrentHotel: false,
+  currentHotel: null,
 });
 
+const BASE_URL = "http://localhost:3000/hotels";
+
 function HotelsProvider({ children }: { children: React.ReactNode }) {
+  const [currentHotel, setCurrentHotel] = useState<SingleHotelTypes | null>(
+    null
+  );
+  const [isLoadingCurrentHotel, setIsLoadingCurrentHotel] =
+    useState<boolean>(false);
   const [searchParams] = useSearchParams();
   const destination = searchParams.get("destination");
   const options = searchParams.get("options");
   const room = options ? JSON.parse(options).Room : [];
   const { isLoading, data: hotels } = useFetch(
-    "http://localhost:3000/hotels",
+    BASE_URL,
     `q=${destination || ""}&accommodates_gte=${room || 1}`
   );
 
+  async function getHotel(id: string | number) {
+    setIsLoadingCurrentHotel(true);
+    try {
+      const response = await fetch(`${BASE_URL}/${id}`);
+      const data = await response.json();
+      setCurrentHotel(data);
+      setIsLoadingCurrentHotel(false);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        toast.error(error.message);
+      } else {
+        toast.error("An unexpected error occurred");
+      }
+      setIsLoadingCurrentHotel(false);
+    }
+  }
+
   return (
-    <HotelsContext.Provider value={{ isLoading, hotels: hotels || [] }}>
+    <HotelsContext.Provider
+      value={{
+        isLoading,
+        hotels: hotels || [],
+        getHotel,
+        isLoadingCurrentHotel,
+        currentHotel,
+      }}
+    >
       {children}
     </HotelsContext.Provider>
   );
