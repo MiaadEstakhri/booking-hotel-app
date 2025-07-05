b37cd7a47c3f4fe376931e7f54afc5b06686ba0e
MiaadEstakhri <miaad8104@gmail.com>
2025-07-05 21:18:17 +0330

    create bookMarkLayout

---
+++ src/components/BookMarkLayout/BookMarksLayout.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Map from "../Map/Map";
+
+function BookMarksLayout() {
+  return (
+    <div className="grid grid-cols-12 mt-8 ">
+      <div className="col-span-12 mb-5 md:mb-0 md:col-span-5 max-h-[800px] overflow-y-auto">
+        <Outlet />
+      </div>
+      <div className="col-span-12 md:col-span-7 w-full h-full">
+        <Map markerLocations={[]} />
+      </div>
+    </div>
+  );
+}
+
+export default BookMarksLayout;
