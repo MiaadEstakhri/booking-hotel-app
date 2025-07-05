bfe7de0ce1d0564fbc89627d3db675779f9cd53a
MiaadEstakhri <miaad8104@gmail.com>
2025-07-05 21:54:59 +0330

    change rename

--- src/components/BookMarkLayout/BookMarksLayout.tsx
+++ src/components/BookMarkLayout/BookMarksLayout.tsx
@@ -1,14 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Map from "../Map/Map";
+import { useBookMark } from "../context/BookMarkProvider";
 
 function BookMarksLayout() {
+  const { bookMarks } = useBookMark();
   return (
     <div className="grid grid-cols-12 mt-8 ">
       <div className="col-span-12 mb-5 md:mb-0 md:col-span-5 max-h-[800px] overflow-y-auto">
         <Outlet />
       </div>
       <div className="col-span-12 md:col-span-7 w-full h-full">
-        <Map markerLocations={[]} />
+        <Map markerLocations={bookMarks} />
       </div>
     </div>
   );
