675ce909493270d5e439d479a97dba182b615be1
MiaadEstakhri <miaad8104@gmail.com>
2025-07-07 22:26:22 +0330

    create add NewBookMark component

--- src/components/AddNewBookMark/AddNewBookMark.tsx
+++ src/components/AddNewBookMark/AddNewBookMark.tsx
@@ -6,37 +6,40 @@ function AddNewBookMark() {
   const [latNum, lngNum] = useUrlLocation();
   console.log(latNum, lngNum);
   return (
-    <form className="flex flex-col gap-5">
-      <div className="flex flex-col gap-1">
-        <label htmlFor="cityName">CityName</label>
-        <input
-          type="text"
-          id="cityName"
-          className="border-2 border-gray-300 rounded-md p-2"
-          placeholder="Enter City Name"
-        />
-      </div>
-      <div className="flex flex-col gap-1">
-        <label htmlFor="country">Country</label>
-        <input
-          type="text"
-          id="country"
-          className="border-2 border-gray-300 rounded-md p-2"
-          placeholder="Enter Country"
-        />
-      </div>
-      <div className="flex justify-between">
-        <button
-          className="p-1 px-2  bg-white border-2 border-gray-300 font-semibold rounded-lg shadow-sm"
-          onClick={() => navigate(-1)}
-        >
-          &larr; Back
-        </button>
-        <button className="p-1 px-3 bg-violet-500 text-white font-semibold rounded-lg shadow-sm">
-          Add
-        </button>
-      </div>
-    </form>
+    <div className="flex flex-col gap-4 me-4">
+      <h2 className="text-2xl font-bold">Add New Bookmark</h2>
+      <form className="flex flex-col gap-5">
+        <div className="flex flex-col gap-1">
+          <label htmlFor="cityName">CityName</label>
+          <input
+            type="text"
+            id="cityName"
+            className="border-2 border-gray-300 rounded-md p-2"
+            placeholder="Enter City Name"
+          />
+        </div>
+        <div className="flex flex-col gap-1">
+          <label htmlFor="country">Country</label>
+          <input
+            type="text"
+            id="country"
+            className="border-2 border-gray-300 rounded-md p-2"
+            placeholder="Enter Country"
+          />
+        </div>
+        <div className="flex justify-between">
+          <button
+            className="p-1 px-2  bg-white border-2 border-gray-300 font-semibold rounded-lg shadow-sm"
+            onClick={() => navigate(-1)}
+          >
+            &larr; Back
+          </button>
+          <button className="p-1 px-3 bg-violet-500 text-white font-semibold rounded-lg shadow-sm">
+            Add
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
 
