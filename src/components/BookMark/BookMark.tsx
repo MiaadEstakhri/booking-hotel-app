59129aa327d884f06ff8ed84de99139ed8bd78d3
MiaadEstakhri <miaad8104@gmail.com>
2025-07-07 22:19:46 +0330

    using currentBookMark for select item and putting a h2

--- src/components/BookMark/BookMark.tsx
+++ src/components/BookMark/BookMark.tsx
@@ -3,16 +3,24 @@ import { useBookMark } from "../context/BookMarkProvider";
 import { Link } from "react-router-dom";
 
 function BookMark() {
-  const { isLoading, bookMarks } = useBookMark();
+  const { isLoading, bookMarks, currentBookmark } = useBookMark();
 
   if (isLoading) return <div>Loading...</div>;
 
   return (
     <div className="flex flex-col gap-4">
+      <h2 className="text-2xl font-bold">
+        Bookmarks List ({bookMarks.length})
+      </h2>
       {bookMarks.map(
         ({ id, countryCode, cityName, country, latitude, longitude }) => (
           <Link key={id} to={`${id}?lat=${latitude}&lng=${longitude}`}>
-            <div key={id} className="p-4 border rounded-xl shadow-md md:me-3">
+            <div
+              key={id}
+              className={`p-4 border rounded-xl shadow-md md:me-3 ${
+                id === currentBookmark?.id ? " border-2 border-violet-500" : ""
+              }`}
+            >
               <ReactCountryFlag countryCode={countryCode} />
               &nbsp; <strong>{cityName}</strong> &nbsp; <span>{country}</span>
             </div>
